6302866f08d46aebf36500c782532d59b6f5d184
imjohnathan <o5786486@gmail.com>
2024-01-09 15:13:12 +0800

    After

--- vite.config.ts
+++ vite.config.ts
@@ -77,19 +77,19 @@ export default defineConfig({
     }),
     imagetools()
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes("node_modules")) {
+  //           return id
+  //             .toString()
+  //             .split("node_modules/")[1]
+  //             .split("/")[0]
+  //             .toString();
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
 });
